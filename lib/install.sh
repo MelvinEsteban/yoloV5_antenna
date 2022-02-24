a9ffadfb01d1f2e639504092df7cf23e3d89f75b
Melvin Esteban <melvin101@live.fr>
2022-02-24 15:41:19 +0100

    Ajout de l'install

---
+++ lib/install.sh
@@ -0,0 +1,7 @@
+# Yolov5
+git clone https://github.com/ultralytics/yolov5
+cd yolov5
+pip install -r requirements.txt
+
+# Add our modification (to detect.py and custom build scripts)
+cp -r modif/yolov5/* yolov5/    
